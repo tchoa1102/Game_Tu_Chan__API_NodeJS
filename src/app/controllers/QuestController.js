8b73ebe5fe6dbd594e172c824c1dfb3aa61fdd11
ChiHoa <kyuseishu1593@gmail.com>
2023-03-21 20:49:08 +0700

    fix plot

--- src/app/controllers/QuestController.js
+++ src/app/controllers/QuestController.js
@@ -84,11 +84,12 @@ class QuestController {
             // console.log(levels, typeOfActivity, typeOfTarget)
 
             // user
+            let totalData = 0
             const avatars = {}
             const user = await User.findById(idUser).populate({ path: 'bag.equipments.equip', })
             const equipmentsOfUser = user.bag.equipments
             const immortalitiesUser = await Immortality.find({user: idUser})
-            await collectAvatars(avatars, immortalitiesUser)
+            totalData += await collectAvatars(avatars, immortalitiesUser)
             // mounted skills
             await mountedSkill(immortalitiesUser)
             // increase
@@ -98,7 +99,7 @@ class QuestController {
             const quest = await Quest.findById(idQuest).populate({ path: 'clusters.immortalities' })
             const cluster = quest.clusters.find( cluster => idCluster == cluster._id.toString() )
             const immortalitiesCluster = cluster.immortalities
-            await collectAvatars(avatars, immortalitiesCluster)
+            totalData += await collectAvatars(avatars, immortalitiesCluster)
             // mounted skills
             await mountedSkill(immortalitiesCluster)
             // increase
@@ -135,8 +136,8 @@ class QuestController {
                     && round < maxRound;
                 round ++) {
                 console.log(Object.keys(mountLeftField).length, Object.keys(mountRightField).length, Object.keys(mountLeftField).length > 0 && Object.keys(mountRightField).length > 0)
-                const you = 1
-                const defense = -1
+                const you = -1
+                const defense = 1
                 const roundHistory = {
                     you: {},
                     defense: {},
@@ -148,7 +149,7 @@ class QuestController {
                 
                 const resultDefense = createPlot(round, whos, defense, rightField, plot, roundHistory, mountRightField, actorFlagDefense, mountLeftField, typeOfActivity, typeOfTarget)
                 actorFlagDefense = resultDefense.actorFlag
-                console.log(mountLeftField, mountRightField)
+                // console.log(mountLeftField, mountRightField)
                 console.log(`\n--------------------------------------\n`)
 
 
@@ -162,7 +163,8 @@ class QuestController {
             } else if (Object.keys(mountLeftField).length == 0 && Object.keys(mountRightField).length == 0) {
                 resultFight = 'Hòa'
             }
-
+            immortalitiesUser
+            immortalitiesCluster
             statesList = statesList.reduce((result, state) => {
                 const newState = {
                     name: state.name,
@@ -173,9 +175,11 @@ class QuestController {
                     amount: 1,
                 }
 
-                if (result[state.name] && result[state.name].amount < 10) {
+                if (result[state.name] && result[state.name].amount < Object.keys(immortalitiesUser).length + Object.keys(immortalitiesCluster).length) {
                     result[state.name].amount += 1
+                    totalData += 1
                 } else if (!result[state.name]) {
+                    totalData += 1
                     result[state.name] = newState
                 }
                 return result
@@ -187,7 +191,8 @@ class QuestController {
                 you: leftImmortalities,
                 defense: rightImmortalities,
             }
-            const skills = collectSkills(skillsList)
+            const { newSkills, totalData: td } = collectSkills(skillsList)
+            totalData += td
             // console.log('\n\n\nresult: ')
             // console.log('Avatars: ', avatars) //done
             // console.log('SkillsList: ', skills) //done
@@ -199,34 +204,41 @@ class QuestController {
             // console.log('Immortality User: ', immortalitiesUser)
             // console.log('Immortality Cluster: ', immortalitiesCluster)
             // console.log(plot, plot.length)
+            plot.forEach((round) => {
+                console.log(round)
+            })
             // plot.forEach((round, index) => console.log(index, round.you.effects, round.defense.effects, '\n'))
             console.log('\n\nThe End!')
             return res.json({
                 avatars,
-                skills,
+                skills: newSkills,
                 states: statesList,
                 status,
                 plot,
                 resultFight,
+                totalData,
             })
         } catch (error) {
             return next(error)
         }
 
         async function collectAvatars(avatars, immortalities) {
+            let totalData = 0
             for(const immortality of immortalities) {
                 const newAvatar = await Avatar.findOne({ name: immortality.avatar })
                 if (newAvatar) {
                     avatars[newAvatar.name] = newAvatar.effects
+                    totalData += Object.keys(newAvatar.effects).length
                 }
             }
+            return totalData
         }
 
         function collectImmortality(immortalities, whos, who) {
             const newImmortalities = {}
             immortalities.forEach(immortality => {
                 const newImmortality = {
-                    index: immortality.index * whos[who],
+                    index: immortality.index * whos[who] * (-1),
                     avatar: immortality.avatar,
                     hp: immortality.status.HP,
                     mp: immortality.status.MP,
@@ -241,6 +253,7 @@ class QuestController {
         }
 
         function collectSkills(skills) {
+            let totalData = 0
             const newSkills = Object.keys(skills).reduce((result, key) => {
                 if (!result[key]) {
                     const newSkill = {
@@ -257,11 +270,12 @@ class QuestController {
                         }
                     }
                     result[key] = newSkill
+                    totalData += result[key].amount
                 }
                 return result
             }, {})
 
-            return newSkills
+            return { newSkills, totalData }
         }
 
         function increase(levels, immortalities, equipments) {
@@ -448,6 +462,7 @@ class QuestController {
                     const targetOfActivitiesObject = (whos[activity.who] < 0) ? enemyImmortalitiesObject : mainImmortalitiesObject
                     // console.log(targetOfActivitiesObject)
                     // console.log(activity.statesBonus)
+
                     const effect = {
                         type: 'skill',
                         name: skillKey,
@@ -665,9 +680,10 @@ class QuestController {
 
         function handleComputedDamage(whos, who, targetImmortalityObject, targetImmortalitiesObject, effect, typeEffect, activity) {
             if (targetImmortalityObject) {
-                console.log(targetImmortalityObject.currentlyStatus.HP, 'type: ', activity.property.type)
+                console.log('name: ', activity.who, ' ', targetImmortalityObject.currentlyStatus.HP, 'type: ', activity.property.type)
                 targetImmortalityObject.currentlyStatus[activity.property.type] -= (-activity.property.value)
 
+                console.log('index: ', targetImmortalityObject.index, ', who: ', who, ', activityWho: ', whos[activity.who], ', kq: ', targetImmortalityObject.index * who * whos[activity.who])
                 effect.objects.push(targetImmortalityObject.index * who * whos[activity.who])
                 effect[typeEffect].push(activity.property.value)
 
